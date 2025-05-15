5a3df7534f180d64b5786a5d745611ee3f9c00d4
jrex4860 <j.rex4860@gmail.com>
2025-05-16 02:06:59 +0900

    main

--- assets/js/steller.js
+++ assets/js/steller.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
 
 	$('#download-btn').on('click', function(){
         var link = document.createElement('a');
-        link.href = 'assets/cv/cv_yeonwoojang.pdf'; // 다운로드할 PDF 경로
+        link.href = 'https://raw.githubusercontent.com/jrex4860/jrex4860.github.io/main/assets/cv/'; // 다운로드할 PDF 경로
         link.download = 'cv_yeonwoojang.pdf'; // 다운로드 파일 이름
         document.body.appendChild(link);
         link.click();
